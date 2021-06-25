122cb096c3da27c2e91a032addb083e2528e34c3
Mirochiu <mirochiu@cht.com.tw>
2021-06-25 16:40:14 +0800

    upgrade gradle and exoplayer to r2.14.1

--- app/src/main/java/jatin/demoapps/com/exoplayer2udpdemo/MainActivity.java
+++ app/src/main/java/jatin/demoapps/com/exoplayer2udpdemo/MainActivity.java
@@ -4,31 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.extractor.Extractor;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ts.DefaultTsPayloadReaderFactory;
 import com.google.android.exoplayer2.extractor.ts.TsExtractor;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.UdpDataSource;
 import com.google.android.exoplayer2.util.TimestampAdjuster;
-
-import java.net.MulticastSocket;
 
 import static com.google.android.exoplayer2.extractor.ts.TsExtractor.MODE_SINGLE_PMT;
 
@@ -55,10 +46,10 @@ public class MainActivity extends AppCompatActivity {
         Uri videoUri = Uri.parse(url);
 
         //Create a default TrackSelector
-        DefaultTrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory());
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector(this, new AdaptiveTrackSelection.Factory());
 
         //Create the player
-        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        player = new SimpleExoPlayer.Builder(this).setTrackSelector(trackSelector).build();
 
         // set player in playerView
         playerView.setPlayer(player);
@@ -68,8 +59,10 @@ public class MainActivity extends AppCompatActivity {
         DataSource.Factory factory = () -> new UdpDataSource(3000, 100000);
         ExtractorsFactory tsExtractorFactory = () -> new TsExtractor[]{new TsExtractor(MODE_SINGLE_PMT,
                 new TimestampAdjuster(0), new DefaultTsPayloadReaderFactory())};
-        MediaSource mediaSource = new ExtractorMediaSource(videoUri, factory, tsExtractorFactory,null,null);
-        player.prepare(mediaSource);
+        MediaItem mediaItem = new MediaItem.Builder().setUri(videoUri).build();
+        ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(factory,tsExtractorFactory).createMediaSource(mediaItem);
+        player.addMediaSource(mediaSource);
+        player.prepare();
 
         // start play automatically when player is ready.
         player.setPlayWhenReady(true);
